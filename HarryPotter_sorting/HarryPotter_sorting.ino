8a7d40c4d1beb246403c6c37e37066b6c12159f6
Vignesh Vishwanathan <vigvish@gmail.com>
2016-02-20 02:38:33 -0600

    Neopixel Support for HP houses
    Added the code that controls color change of neopixels based on input
    from Hat Chat Bot

--- HarryPotter_sorting/HarryPotter_sorting.ino
+++ HarryPotter_sorting/HarryPotter_sorting.ino
@@ -1,43 +1,51 @@
+#include <Servo.h>
 #include <Adafruit_NeoPixel.h>
 
 int val;
-
-//      #define PIN 6
+      #define PIN 6
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//Adafruit_NeoPixel strip = Adafruit_NeoPixel(2, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(2, PIN, NEO_GRB + NEO_KHZ800);
     
 
 void setup() {
   Serial.begin(9600); 
-  //strip.begin();
- // strip.show(); // Initialize all pixels to 'off'
+  strip.begin();
+  strip.setPixelColor(0, 75, 0, 130);
+  strip.show();
 }
 
 void loop() {
-   if (Serial.available()){
+  if (Serial.available()){
     val = Serial.read();
     if (val == '0'){
-      Serial.print("Ravenclaw");
-      Serial.println();
+      Serial.print("Rawenclaw");
+      strip.setPixelColor(0, 0, 0, 150);
+      strip.setPixelColor(1, 0, 0, 150);
+      strip.show();
     }
     if (val == '1'){
       Serial.print("Sytherin");
-      Serial.println();
+      strip.setPixelColor(0, 0, 150, 0);
+      strip.setPixelColor(1, 0, 150, 0);
+      strip.show();
     }
     if (val == '2'){
     Serial.print("Gryffindor");
-    Serial.println();
+    strip.setPixelColor(0, 150, 0, 0);
+    strip.setPixelColor(1, 150, 0, 0);
+    strip.show();
     }
     if (val == '3'){
       Serial.print("Hufflepuff");
-      Serial.println();
+      strip.setPixelColor(0, 255, 150, 0);
+      strip.setPixelColor(1, 255, 150, 0);
+      strip.show();
     }
    }
 }
+

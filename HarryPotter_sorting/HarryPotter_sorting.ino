153095674c7c58f867ef67b28fc294c703cdb2f6
Vignesh Vishwanathan <vigvish@gmail.com>
2016-02-20 21:46:02 -0600

    Light Updates
    - Regular light color
    - Controlled movement of motors with speech
    - Updated so that eyes light up to house color only when mentioning the
    house color

--- HarryPotter_sorting/HarryPotter_sorting.ino
+++ HarryPotter_sorting/HarryPotter_sorting.ino
@@ -18,8 +18,8 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(2, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   Serial.begin(9600); 
   strip.begin();
-  strip.setPixelColor(0, 75, 0, 130);
-  strip.setPixelColor(1, 75, 0, 130);
+  strip.setPixelColor(0, 200, 200, 200);
+  strip.setPixelColor(1, 200, 200, 200);
   strip.show();
   moveservo.attach(9);
   moveservo.write(45);
@@ -66,6 +66,11 @@ void movemotor(){
   pos = 135;
   moveservo.write(pos);
   delay(5); 
+  pos = 45;
+  moveservo.write(pos);              // tell servo to go to position in variable 'pos'
+  delay(5);
+  pos = 135;
+  moveservo.write(pos);
 }
 
 void loop() {
@@ -73,7 +78,10 @@ void loop() {
     sorting();
    }
   if (val == '4'){
-    movemotor(); 
+    movemotor();
+    val = 10; 
   }
+    strip.setPixelColor(0, 200, 200, 200);
+    strip.setPixelColor(1, 200, 200, 200);
 }
 
